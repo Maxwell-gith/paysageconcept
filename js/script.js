e75db2870e804217ee449620131b276b5f0863da
Maxime <herymaxime@gmail.com>
2022-11-21 20:47:41 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function burgerMenu () {
+    const navbar = document.querySelector('.navbar');
+    const burger = document.querySelector('.navbar__burger');
+    burger.addEventListener('click', function () {
+        navbar.classList.toggle('show-navbar');
+        // if (navbar.classList.contains('show-navbar')) {
+        //     nonScroll();
+        // }else{
+        //     Scroll()
+        // }
+    })
+}
+
+function closeBurgerMenu () {
+    const links = document.querySelector('.navbar__links');
+    const navBar = document.querySelector('.navbar');
+    links.addEventListener('click', function () {
+        navBar.classList.remove('show-navbar');
+    })
+}
