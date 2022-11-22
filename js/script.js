25b318fc92e5f70c49df24a2a440b53a06897758
Maxime <herymaxime@gmail.com>
2022-11-22 23:34:37 +0100

    grosse avancée

--- js/script.js
+++ js/script.js
@@ -19,5 +19,30 @@ function closeBurgerMenu () {
     })
 }
 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
 burgerMenu();
 closeBurgerMenu();
